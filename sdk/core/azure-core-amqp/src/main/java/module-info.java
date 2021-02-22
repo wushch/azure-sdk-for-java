d7cd58b35152ba3dbe5f3f9b91018a030b9cd371
Connie <conniey@microsoft.com>
2021-02-22 13:53:22 -0800

    Remove exports from module-info.

--- sdk/core/azure-core-amqp/src/main/java/module-info.java
+++ sdk/core/azure-core-amqp/src/main/java/module-info.java
@@ -11,14 +11,4 @@ module com.azure.core.amqp {
     exports com.azure.core.amqp;
     exports com.azure.core.amqp.models;
     exports com.azure.core.amqp.exception;
-
-    // FIXME this should not be a long-term solution
-    exports com.azure.core.amqp.implementation to
-        com.azure.messaging.eventhubs,
-        com.azure.messaging.servicebus.implementation,
-        com.azure.messaging.servicebus;
-
-    exports com.azure.core.amqp.implementation.handler to
-        com.azure.messaging.eventhubs,
-        com.azure.messaging.servicebus;
 }
